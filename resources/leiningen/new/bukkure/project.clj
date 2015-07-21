f3253d22eee570fe81366e4f2458e741638e7b9b
SevereOverfl0w <overfl0w@narna.co>
2015-07-21 13:40:55 +0100

    bump bukkure version.

--- resources/leiningen/new/bukkure/project.clj
+++ resources/leiningen/new/bukkure/project.clj
@@ -1,5 +1,5 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "A bukkure plugin to..."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [bukkure "0.1.0-SNAPSHOT"]]
+                 [bukkure "0.2.1-SNAPSHOT"]]
   :java-source-paths ["javasrc"])
