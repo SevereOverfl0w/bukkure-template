6ee52d73d2363919784fecfac68d7a581fe6c761
Dominic Monroe <iam@dominic.io>
2016-01-03 13:39:54 +0000

    Bump version, make compatible with provided bukkit (the future tm)

--- resources/leiningen/new/bukkure/project.clj
+++ resources/leiningen/new/bukkure/project.clj
@@ -1,5 +1,8 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "A bukkure plugin to..."
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [bukkure "0.4.1-SNAPSHOT"]]
+                 [bukkure "0.4.2-SNAPSHOT"]]
+  :repositories [["spigot-repo" "https://hub.spigotmc.org/nexus/content/groups/public/"]]
+  :profiles {:provided
+             {:dependencies [[org.bukkit/bukkit "1.8.8-R0.1-SNAPSHOT"]]}}
   :java-source-paths ["javasrc"])
